a7a2bb06d8046475aac800c5bbd490d3bd803b26
Ayush Kumar <94730528+Akayush-17@users.noreply.github.com>
2023-05-25 19:31:56 +0530

    added video layout

--- src/components/video/Video.js
+++ src/components/video/Video.js
@@ -1,9 +1,32 @@
 import React from 'react'
 import "./_video.scss"
 
-function Video() {
+import {AiFillEye} from 'react-icons/ai'
+
+const Video=()=> {
   return (
-    <div>Video</div>
+    <div className='video'>
+      <div className='video__top'>
+        <img src="https://i.ytimg.com/vi/s2skans2dP4/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCUOcaO5GQwV8N0gGtT-Wtd6vE_BQ" alt=""/>
+        <span>05:43</span>
+      </div>
+      <div className='video__title'>
+        Learn React in 30 DAYS!!
+      </div>
+      <div className='video__details'>
+        <span>
+         <AiFillEye/> 2.1m Views • 
+        </span>
+        <span> 5 days ago</span>
+      </div>
+      <div className='video__channel'>
+        <img src="https://yt3.ggpht.com/ytc/AGIKgqMc9groeAn4S4GzDo9NXd93oXu3UXVu5D9MsGbiUw=s68-c-k-c0x00ffffff-no-rj" alt="" />
+        <p>Deep Bug Divers</p>
+        
+      </div>
+    </div>
+    
+    
   )
 }
 
