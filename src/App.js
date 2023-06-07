ee80cf01c83216c16412898554452b7899f3a0b7
Ayush Kumar <94730528+Akayush-17@users.noreply.github.com>
2023-06-07 23:09:26 +0530

    chnges

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ const App = () => {
     <Switch>
     <Route path="/" exact>
       <Layout>
-        <HomeScreen/>
+        <HomeScreen2/>
       </Layout>
     </Route>
     <Route path='/auth'>
